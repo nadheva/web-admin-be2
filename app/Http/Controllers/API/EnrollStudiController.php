8900d6c5df294aca4e92b402da2432b8cba8a342
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-04-11 15:12:34 +0700

    push

--- app/Http/Controllers/API/EnrollStudiController.php
+++ app/Http/Controllers/API/EnrollStudiController.php
@@ -44,15 +44,16 @@ class EnrollStudiController extends Controller
     public function enroll_program($id)
     {
         // $enroll = EnrollStudi::select('user_id')->where($id, EnrollStudi::get()->kelas()->program_id)->get();
-        $user = User::all();
-        $enroll = EnrollStudi::select('user_id')->where('id', '=', $user->studi->kelas->program_id)->get();
+        $user = User::get();
+        $enroll = EnrollStudi::select('user_id')->get();
+        $enrolluser = $enroll->where('id', $user->studi->kelas->program_id)->get();
         // $enroll = EnrollStudi::whereHas('user_id', function ($user) {
         //     return $user->where('id', '=', $user->studi->kelas->program_id);
         // })->get();
         return response()->json([
             "error" => false,
             "success" => true,
-            "status" => $enroll
+            "status" => $enrolluser
         ]);
     }
 
