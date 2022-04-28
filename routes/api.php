d9629635a3ff7b245658a78d3b5a5a259aec1e4c
Egy Yuda Nugraha <egyyudanugraha@gmail.com>
2022-04-28 11:07:52 +0700

    Fix Dokumen konsultasi, Iklan, Job Channel (#25)
    * Tesing new url
    
    * Delete laravel demo
    
    * Add API Dokumen Konsultasi
    
    * fix resource API & add API Jadwal Matkul
    
    * delete sanctum
    
    * Fix Dokumen Konsultasi, Iklan, Job Channel
    
    * Merge from master to Branch Yuda (#23)
    
    * Delete sanctum (#22)
    
    * Tesing new url
    
    * Delete laravel demo
    
    * Add API Dokumen Konsultasi
    
    * fix resource API & add API Jadwal Matkul
    
    * delete sanctum
    
    * push
    
    * revisi controller enroll & kelas
    
    * revisi
    
    * revisi
    
    Co-authored-by: Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
    Co-authored-by: sar <58721335+erzaffasya@users.noreply.github.com>
    
    Co-authored-by: Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
    Co-authored-by: sar <58721335+erzaffasya@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -120,13 +120,8 @@ Route::resource('iklan', IklanController::class);
 Route::get('/job_kerja', [JobChannelController::class, 'job_kerja']);
 Route::get('/job_magang', [JobChannelController::class, 'job_magang']);
 Route::get('/job_project', [JobChannelController::class, 'job_project']);
-Route::get('/jobChannel', [JobChannelController::class, 'index']);
-Route::post('/jobChannel', [JobChannelController::class, 'store']);
-Route::get('/jobChannel/{id}', [JobChannelController::class, 'show']);
-Route::get('/jobChannel/{id}/download', [JobChannelController::class, 'download']);
 Route::get('/jobChannel/{id}/view', [JobChannelController::class, 'view']);
-
-
+Route::resource('/jobChannel', JobChannelController::class);
 
 //Route Quiz
 Route::get('/quiz', [QuizController::class, 'index']);
