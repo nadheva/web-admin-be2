b5f16c06091f39c2acd489038cf36aa6b2466377
LearningKG <learning.kampusgratis1@gmail.com>
2022-05-28 00:40:45 +0700

    up

--- app/Http/Controllers/API/MataKuliahController.php
+++ app/Http/Controllers/API/MataKuliahController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MataKuliahResource;
 use App\Http\Resources\MataKuliahCollection;
+use App\Http\Resources\UserStudy;
+use App\Models\Pertemuan;
+use App\Models\UserDokumen;
+use App\Models\UserVideo;
 
 class MataKuliahController extends Controller
 {
@@ -34,5 +38,21 @@ class MataKuliahController extends Controller
     {
         $meet = MataKuliah::find($id);
         return new MataKuliahResource($meet);
+    }
+
+    public function userModule($id){
+        $id = 1;
+        $user_id = 7;
+        $meet = Pertemuan::with('mataKuliah')->where('mata_kuliah_id', $id)->first();
+        $video = $meet->kontenVideo_id;
+        $dokumen = $meet->kontenDokumen_id;
+        $user_video = UserVideo::where('user_id', $user_id)->where('enroll_mata_kuliah_id', 1)->get();
+        $user_dokumen = UserDokumen::where('user_id', $user_id)->where('enroll_mata_kuliah_id', 1)->get();
+
+        // $data = new Collection([
+        //     $meet, $video, $dokumen, $user_video, $user_dokumen
+        // ]);
+
+        return new UserStudy($meet);
     }
 }
